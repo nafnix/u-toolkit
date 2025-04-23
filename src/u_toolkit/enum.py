5c9e4cae4c42a1272c539cf53d07618f7d68f28e
nafnix <uwu@nafnix.com>
2025-04-23 22:56:29 +0800

    feat: `TitleEnum` 剔除首尾的空字符

--- src/u_toolkit/enum.py
+++ src/u_toolkit/enum.py
@@ -15,29 +15,29 @@ __all__ = [
 
 class NameEnum(StrEnum):
     @staticmethod
-    def _generate_next_value_(name, *_, **__) -> str:
+    def _generate_next_value_(name: str, *_, **__) -> str:
         return name
 
 
 class PascalEnum(StrEnum):
     @staticmethod
-    def _generate_next_value_(name, *_, **__) -> str:
+    def _generate_next_value_(name: str, *_, **__) -> str:
         return to_pascal(name)
 
 
 class CamelEnum(StrEnum):
     @staticmethod
-    def _generate_next_value_(name, *_, **__) -> str:
+    def _generate_next_value_(name: str, *_, **__) -> str:
         return to_camel(name)
 
 
 class SnakeEnum(StrEnum):
     @staticmethod
-    def _generate_next_value_(name, *_, **__) -> str:
+    def _generate_next_value_(name: str, *_, **__) -> str:
         return to_snake(name)
 
 
 class TitleEnum(StrEnum):
     @staticmethod
-    def _generate_next_value_(name, *_, **__) -> str:
-        return name.replace("_", " ").title()
+    def _generate_next_value_(name: str, *_, **__) -> str:
+        return name.replace("_", " ").strip().title()
